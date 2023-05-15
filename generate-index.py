6b6a3f14c10402e47bd9a5c780f19109693e2670
legitShivam <shiv.05cs@gmail.com>
2023-05-15 17:43:00 +0530

    chore(scripts): update index style - remove hyperlinks from list items - remove topic name item and 'Table of Contents' item from list - update docstrings

--- generate-index.py
+++ generate-index.py
@@ -8,16 +8,24 @@ import re
 
 def extract_table_of_contents(md_file_path):
     """
-    The extract_table_of_contents function takes a markdown file path as input and returns the table of contents
-    of that markdown file. The table of contents is returned as a list, where each element in the list is one line
-    in the table of contents.
+    The extract_table_of_contents function takes a markdown file path as input and returns a list of strings.
+    The first element in the list is an empty string, and each subsequent element is the title of one section in the markdown file.
 
-    :param md_file_path: Specify the path to the markdown file
+
+    :param md_file_path: Open the file and read its contents
     :return: A list of strings
     """
     with open(md_file_path, 'r') as file:
         content = file.read()
-    toc = re.findall(r'- \[.*\]\(#.*\)', content)
+        name = file.name
+    tmp = re.findall(r'- \[(.*?)\]\(#.*\)', content)
+    toc = []
+    for item in tmp:
+        if (item.lower() in name.lower()) or (item in "Table of Contents"):
+            continue
+        else:
+            toc.append(item)
+    toc.insert(0, "")
     return toc
 
 
@@ -25,22 +33,23 @@ def create_index_elements(directory_path, index=[]):
     """
     The create_index_elements function takes a directory path and an index list as arguments.
     It then iterates through the files in the directory, ignoring any hidden files or READMEs.
-    If it finds a markdown file, it extracts its table of contents and appends that to the index list.
-    If it finds another directory, it recursively calls itself on that subdirectory.
+    If it finds a markdown file, it extracts its table of contents and adds that to the index list along with 
+    the file's name (capitalized) and link to itself. If it finds another directory, it recursively calls itself on that 
+    directory.
 
-    :param directory_path: Pass the path of the directory to be indexed
-    :param index: Store the index elements
-    :return: A list of strings, each string
+    :param directory_path: Specify the directory to scan for markdown files
+    :param index: Pass the index list to the function
+    :return: A list of strings
     """
     for entry in os.scandir(directory_path):
         if entry.name.startswith('.') or entry.name.startswith('README'):
             continue
         if entry.is_file() and entry.name.endswith('.md'):
             toc = extract_table_of_contents(entry.path)
-            toc = "\n".join(toc)
+            toc = "\n- ".join(toc)
             file_name = entry.name.replace('.md', '').capitalize().strip()
             index.append(
-                f'\n<details>\n  <summary> <a href="{entry.path}"> {file_name} </a> </summary>\n\n{toc}\n\n</details>')
+                f'\n<details>\n  <summary> <a href="{entry.path}"> {file_name} </a> </summary>\n{toc}\n\n</details>')
         elif entry.is_dir():
             create_index_elements(entry, index=index)
     return index
@@ -48,11 +57,12 @@ def create_index_elements(directory_path, index=[]):
 
 def generate_readme_file(index):
     """
-    The generate_readme_file function generates a README.md file for the GitBook repository,
-    which contains an index of all the topics/fields that are covered in this repository.
+    The generate_readme_file function takes in a list of strings, which are the names of all the directories within
+    the current directory. It then writes those strings to a file called README.md, which is used by GitBook as an index
+    for all the notes contained within this repository.
 
-    :param index: Write the index of the gitbook to readme
-    :return: None, because it doesn't have a return statement
+    :param index: Write the index of all the notes to a file
+    :return: The index of the readme file
     """
     with open('README.md', 'w') as file:
         file.write("""\
